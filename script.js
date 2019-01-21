f05ecffd499aa6eb0bc7a32b27f6d9317f7395ab
Louis Beul <l.beul@outlook.com>
2019-01-21 17:48:23 +0100

    ES6 conformity and html fixes

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 //H3 Element that has to display the code:
-var cssOutput = document.querySelector("h3");
+const cssOutput = document.querySelector("h3");
 //Body Element:
-var body = document.getElementById("background");
+const body = document.getElementById("background");
 //Array of the two color inputs
-var colors = document.getElementsByTagName("input");
+const colors = document.getElementsByTagName("input");
 
-function setGradient() {
+//Gradient Color Selection &&
+//CSS code display
+const setGradient = () => {
 	body.style.background = 
 		"linear-gradient(to right, " 
 		+ colors[0].value 
